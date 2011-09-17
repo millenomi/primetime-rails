96c8f9dc50eb1db734642392f5d33fc2659d2fa4
Emanuele "∞" Vulcano <me@infinite-labs.net>
2011-09-17 10:10:32 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Primetime::Application.config.secret_token = '3485fb99dacb935ed0ace2219fac5154a2e7c4b2fe25e91a50630670c794e888ac5c707575a36c8aa6a2ce4f8adc5da98374c38452616f2949d1681a32fb7054'
